4b175a9690b156d81d615666b747d461a8bfaac3
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-21 16:37:49 +0600

    appointmentOption api modified

--- index.js
+++ index.js
@@ -24,18 +24,65 @@ async function run(){
         const appointmentOptionCollection=client.db('doctorsPortal-practice').collection('appointmentOptions')
         const bookingsCollection=client.db('doctorsPortal-practice').collection('bookings')
 
-        app.get('/appointmentOptions',async(req,res)=>{
-            const date=req.query.date
-            const query={}
-            const options=await appointmentOptionCollection.find(query).toArray()
-            const bookingQuery={appointmentDate:date}
-            const alreadyBooked=await bookingsCollection.find(bookingQuery).toArray()
-            options.forEach(option=>{
-                const optionBooked=alreadyBooked.filter(book=>book.treatment===option.name)
-                const bookSlots=optionBooked.map(book=>book.slot)
-            })
-            res.send(options)
-        });
+        // app.get('/appointmentOptions',async(req,res)=>{
+        //     const date=req.query.date
+        //     const query={}
+        //     const options=await appointmentOptionCollection.find(query).toArray()
+        //     const bookingQuery={appointmentDate:date}
+        //     const alreadyBooked=await bookingsCollection.find(bookingQuery).toArray()
+        //     options.forEach(option=>{
+        //         const optionBooked=alreadyBooked.filter(book=>book.treatment===option.name)
+        //         const bookSlots=optionBooked.map(book=>book.slot)
+        //         const remainingSlots=option.slots.filter(slot=> !bookSlots.includes(slot))
+        //         option.slots=remainingSlots
+        //     })
+        //     res.send(options)
+        // });
+         
+
+        app.get('/v2/appointmentOptions',async(req,res)=>{
+             const date=req.query.date 
+             const options=await appointmentOptionCollection.aggregate([
+                {
+                    $lookup:{
+                        from:'bookings',
+                        localField:'name',
+                        foreignField:'treatment',
+                        pipeline:[{
+                            $match:{
+                                $expr:{
+                                    $eq:['$appointmentDate',date]
+                                }
+                            }
+                        }],
+                        as:'booked'
+                    }
+                },
+                {
+                    $project:{
+                        name:1,
+                        slots:1,
+                        booked:{
+                            $map:{
+                                input:'$booked',
+                                as:'book',
+                                in:'$$book.slot'
+                            }
+                        }
+                    }
+                },
+                {
+                    $project:{
+                        name:1,
+                        slots:{
+                            $setDifference:['$slots','$booked']
+                        }
+                    }
+                }
+             ]).toArray();
+             res.send(options)
+        })
+
 
         app.post('/bookings',async(req,res)=>{
             const booking=req.body 

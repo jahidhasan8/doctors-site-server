780fe4a6fb6773aed9c5d85639587ac781d2b328
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-22 21:14:07 +0600

    users api created

--- index.js
+++ index.js
@@ -147,6 +147,14 @@ async function run(){
             const users=await usersCollection.find(query).toArray()
             res.send(users)
         })
+
+        app.get('/users/admin/:email',async(req,res)=>{
+            const email=req.params.email
+            const query={email}
+            const user=await usersCollection.findOne(query)
+            res.send({isAdmin:user?.role==='admin'})
+
+        })
         app.post('/users',async(req,res)=>{
             const user=req.body 
             const result =await usersCollection.insertOne(user)
